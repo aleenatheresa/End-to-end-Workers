71758540057ede88515b08a694601bb17758dfc0
aleenatheresa <aleenatheresageorge@mca.ajce.in>
2020-10-14 21:34:23 +0530

    partial complete

---
+++ DbConnection.php
@@ -0,0 +1,4 @@
+<?php 
+$con=mysqli_connect("localhost","root","","Store1")
+or die("couldn't connect");
+?>
---
+++ usertype.php
@@ -0,0 +1,44 @@
+<?php
+
+session_start();
+$con=mysqli_connect("localhost","root","","projectdb");
+$password=$_POST["pass"];
+$username=$_POST["name"];
+$sql="select * from tbl_login where uname='$username' and password='$password'";
+$result=mysqli_query($con,$sql);
+$n=mysqli_num_rows($result);
+
+$_SESSION['uname']=$username;
+if($n > 0)
+{
+	while($a=mysqli_fetch_array($result))
+	{
+		if($a['role_id']=='1')
+		{
+			header("location:admin_index.html");
+		}
+		elseif($a['role_id']=='2')
+		{
+			header("location:cust_indext.html");
+		}
+		elseif($a['role_id']=='3')
+		{
+			header("location:emp_index.html");
+		}
+		elseif($a['role_id']=='4')
+		{
+			header("location:sp_indext.html");
+		}
+	
+	}
+}
+else
+{
+?>
+<script>alert("no user found");
+location.href ="login.html";
+</script>
+<?php
+}
+mysqli_close($con);
+?>
